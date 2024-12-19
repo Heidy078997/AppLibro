d19a54ad9b823d30b2d23d61d66067a0f323beb2
HeidyG <heidygonzalezg.97@gmail.com>
2024-12-19 10:14:37 -0300

    Se añade diseño basico a pantalla login

--- app/src/main/java/com/heidygonzalez/applibros/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/heidygonzalez/applibros/ui/screens/LoginScreen.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -24,11 +22,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.heidygonzalez.applibros.model.LoginRequest
 import kotlinx.coroutines.delay
+
 
 @Composable
 fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel) {
@@ -52,6 +56,21 @@ fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // Título de la app
+        Text(
+            text = "Lyrica Books",
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.onBackground, // Se adapta al tema (negro/blanco)
+                fontSize = 40.sp, // Tamaño grande para destacar
+                fontWeight = FontWeight.ExtraBold, // Para hacerlo más llamativo
+                fontFamily = FontFamily.Cursive // Tipo de letra estético (puedes cambiarlo según gusto)
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp) // Espaciado para separar el título del contenido
+        )
+
         var username by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
 
